f1d8c72cdf1b81766f2f7541dfcc87a054701890
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-01-04 18:13:35 +0100

    login done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.login');
 });
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+});
